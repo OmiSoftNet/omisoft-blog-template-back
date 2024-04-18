38ab2eeb79f01f1c18ad72c4cdb200f73a6576c6
Yevhen S <eresid@gmail.com>
2024-04-19 01:18:54 +0300

    updated express loader, added morgan

--- app.ts
+++ app.ts
@@ -1,18 +1,15 @@
-import express from "express";
-import expressLoader from "./src/loaders/express.loader";
 import mongooseLoader from "./src/loaders/mongoose.loader";
 import CONFIG from "./src/config";
+import loadServer from "./src/loaders/express.loader";
 
 const port = CONFIG.PORT;
 
 const startServer = () => {
-  const app = express();
-
-  expressLoader(app);
+  const app = loadServer();
   mongooseLoader();
 
   app.listen(port, () => {
-    console.log(`[*] Server successfully started at: ${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   });
 };
 
--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import morgan from "morgan";
 import swaggerUI from "swagger-ui-express";
 import swaggerDoc from "../../swagger.json";
 import routes from "../routes";
@@ -6,19 +7,28 @@ import errorHandlerMiddleware from "../middleware/errorHandler.middleware";
 import corsMiddleware from "../middleware/cors.middleware";
 import authVerifyMiddleware from "../middleware/authVerify.middleware";
 import { API_ROUTES } from "../config/apiRoutes";
+import { isDevEnv } from "../utils/EnvChecker";
 
-export default (app: Express) => {
+export default function loadServer() {
+  const app: Express = express();
+
   app.use(corsMiddleware);
   app.use(express.json());
   app.use(authVerifyMiddleware);
 
+  if (isDevEnv()) {
+    app.use(morgan("tiny"));
+
+    app.use(
+      API_ROUTES.SERVICES.SWAGGER,
+      swaggerUI.serve,
+      swaggerUI.setup(swaggerDoc)
+    );
+  }
+
   routes(app);
 
-  app.use(
-    API_ROUTES.SERVICES.SWAGGER,
-    swaggerUI.serve,
-    swaggerUI.setup(swaggerDoc)
-  );
-
   app.use(errorHandlerMiddleware);
-};
+
+  return app;
+}
